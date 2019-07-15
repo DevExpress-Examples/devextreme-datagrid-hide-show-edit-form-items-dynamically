077e60650d8fd87a56d64a9c6d997658383eea8d
kurchenko.artem <artem.kurchenko@devexpress.com>
2019-07-15 18:37:15 +0300

    Add  the .NET Core approach

---
+++ NetCore/NetCore/Controllers/StatesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.Models;
+
+namespace NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : Controller
+    {
+        [HttpGet]
+        public object Get([FromBody]DataSourceLoadOptions loadOptions)
+        {
+            return DataSourceLoader.Load(SampleData.GetStates(), loadOptions);
+        }
+    }
+}
