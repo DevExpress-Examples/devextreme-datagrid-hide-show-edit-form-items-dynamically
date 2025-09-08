cbc22fe140e98e9d364f6635aa258e28702e48a9
Tom <artem.kurchenko@devexpress.com>
2025-09-08 17:57:31 +0400

    Angular: Enhanced TypeScript with DevExtreme Angular types

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,70 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import { DxDataGridComponent, type DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import config from 'devextreme/core/config';
+
+import type { DxFormTypes } from 'devextreme-angular/ui/form';
+import type { DxTextAreaTypes } from 'devextreme-angular/ui/text-area';
+import { DataService, type Employee, type State } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [DataService],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild(DxDataGridComponent) dataGrid!: DxDataGridComponent<Employee, number>;
 
-  counter = 0;
+  dataSource: Employee[];
 
-  buttonText = 'Click count: 0';
+  states: State[];
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  notesEditorProperties: DxTextAreaTypes.Properties;
+
+  constructor(service: DataService) {
+    config({
+      editorStylingMode: 'filled',
+    });
+    this.dataSource = service.getEmployees();
+    this.states = service.getStates();
+    this.notesEditorProperties = { height: 100 };
+  }
+
+  isHomeAddressGroup(item: DxFormTypes.GroupItem): boolean {
+    return Boolean(item?.itemType === 'group' && item?.caption === 'Home Address');
+  }
+
+  customizeItem = (item: DxFormTypes.Item): void => {
+    if (this.isHomeAddressGroup(item)) {
+      const gridInstance = this.dataGrid.instance;
+      if (!gridInstance) return;
+
+      const editing = gridInstance.option('editing');
+      if (!editing?.editRowKey) return;
+
+      const rowIndex = gridInstance.getRowIndexByKey(editing.editRowKey);
+      item.visible = gridInstance.cellValue(rowIndex, 'AddressRequired');
+    }
+  };
+
+  onEditorPreparing(e: DxDataGridTypes.EditorPreparingEvent<Employee, number>): void {
+    if (e.dataField === 'LastName' && e.parentType === 'dataRow') {
+      e.editorOptions.disabled = Boolean(e.row?.data?.FirstName === '');
+    }
+  }
+
+  onInitNewRow(e: DxDataGridTypes.InitNewRowEvent<Employee, number>): void {
+    e.data.AddressRequired = false;
+    e.data.FirstName = '';
+  }
+
+  setCellValue(
+    this: DxDataGridTypes.Column,
+    newData: Employee,
+    value: number,
+    currentRowData: Employee,
+  ): void {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this.defaultSetCellValue?.(newData, value, currentRowData);
   }
 }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxButtonModule,
+    DxDataGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
