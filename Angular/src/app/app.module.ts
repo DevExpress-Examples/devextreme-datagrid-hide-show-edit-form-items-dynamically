fb583a9793f95323a67343a14251705c8a7d1c0d
Tom <artem.kurchenko@devexpress.com>
2025-09-09 15:00:05 +0400

    fix angular UI

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxTextAreaModule } from 'devextreme-angular/ui/text-area';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     DxDataGridModule,
+    DxTextAreaModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
